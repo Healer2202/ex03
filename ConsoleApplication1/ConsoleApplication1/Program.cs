54ae94419d8be2ecd9b13ec7f908d7f8a200383b
Healer2202 <0962392354Hl>
2019-09-25 20:20:33 +0700

    ex03

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,14 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ex03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlddddddd");
+            int number = Convert.ToInt32(Console.ReadLine());
+            DrawPynamid(number, 'x');
+            Console.WriteLine();
+
         }
-        
+        static void CreateHorizon(int number, char c)
+        {
+            //plus space then print *
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write(c);
+            }
+        }
+        static void CreateVertical(int number, char c)
+        {
+            //plus space then print *
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        static void Dline(int number, char c)
+        {
+            //plus space then print *
+            for (int i = 0; i < number; i++)
+            {
+                CreateHorizon(i, ' ');
+                Console.WriteLine(c);
+            }
+        }
+        static void DrawPynamid(int number, char c)
+        // line 1 : n-1 space / 1 star
+        // line 2 : n-2 space/ 1 star, 1 space/1 star
+        // line 3 : n-3 space/ 1 star, 3 space/1 star
+        // last line 2n-1 star 
+        {
+            //first line
+            CreateHorizon(number - 1, ' '); CreateHorizon(1, c); Console.WriteLine();
+            // i-th line           
+            int line = 1, space = number + 1;
+            for (int i = 1; i < number; i++)
+            {
+                line = line + 1;
+                CreateHorizon(number - line, ' '); CreateHorizon(1, c);
+                // we got space = number+1 - 2, because Value (number - space) must grow 2 time/ 1 it's only a oDD 
+                space = space - 2;
+                CreateHorizon(number - space, ' '); CreateHorizon(1, c); Console.WriteLine();
+            }
+            // last line
+            CreateHorizon(number * 2 - 1, c);
+        }
     }
 }
